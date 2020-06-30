ffb7ccf81e974b6b66de9fdef9c6e592a936e236
Erik Rehnberg <erik.m.rehnberg@gmail.com>
2020-06-29 23:13:00 -0400

    still errors but I tried

--- R/gofarR_functions.R
+++ R/gofarR_functions.R
@@ -1,9 +1,3 @@
-##Peloton scripts
-library(dplyr)
-
-## Initialize workout_data_interface to make things simpler
-workout_data_interface <- i_like_data
-
 ##
 
 # Reads in <username>_workouts.csv and sets a number of values in the global environment
@@ -17,17 +11,16 @@ workout_data_interface <- i_like_data
 #'
 #' @param display_name Your display name on the leaderboard. If you have already set it as a string in a variable named \code{leaderboard_name}, it will use that by default
 #'
-#' @examples
-#' setup_peloton_data()
-#' setup_peloton_data(leaderboard_name)
-#' setup_peloton_data(display_name = "leaderboardName")
-#'
 #' @return Returns a dataframe, workout_data_interface
 #'
 #' @export
 #'
-setup_peloton_data <- function(display_name = leaderboard_name) {
-  workout_data_interface <<- read.csv(paste0(display_name,"_workouts.csv"))
+setup_peloton_data <- function(display_name = "None") {
+  if (display_name == "None") {
+    ouch <- "Bad input"
+    ouch
+  }
+  else workout_data_interface <<- read.csv(paste0(display_name,"_workouts.csv"))
 }
 
 #' @name top_instructor
@@ -56,32 +49,30 @@ top_instructor <- function(discipline = "All") {
 
 #' @title Cycling Summary
 #' @name cycling_summary()
-#' @description
+#' @description This function summarizes cycling data
 #' @usage cycling_summary(data = workout_data_interface, type = "All", min_time = 11, min_output = 50)
 #'
 #' @param data - A formatted dataframe read in using setup_peloton_data. Defaults to a variable, "workout_data_interface", that is the output from that function.
 #' @param type Which type of
-#' @param min_time The minimum amount of time to be summarized, used to exclude short rides, either ones that were aborted or warmups/cooldowns. Defaults to 11 minutes to exclude all rides 10  minutes or shorter
+#' @param min_time The minimum amount of time to be summarized, used to exclude short rides, either ones that were aborted or warmups/cooldowns.
 #' @param min_output The minimum output to be included in the summary. Defaults to 50 KJ
 #' @examples
-#'
 #' cycling_summary()
-#' cycling_summary(type = "Climb", min_time = 11, min_ouput = 200)
+#' cycling_summary(type = "Climb", min_time = 11)
 #'
 #' @return
 #' @export
 
-cycling_summary <- function(data = workout_data_interface, type = "All", min_time = 11, min_output = 50) {
+cycling_summary <- function(data = workout_data_interface,
+                            type = "All",
+                            min_time = 11,
+                            min_output = 50) {
   # Selects only cycling rides which conform to min_time and min_output
-  data <- data%>%
-    filter(Fitness.Discipline == "Cycling")%>%
-    filter(Length..minutes. >= min_time)%>%
-    filter(Total.Output >= min_output)
+  data <- dplyr::filter(data, Fitness.Discipline == "Cycling" & Length..minutes. >= min_time & Total.Output >= min_output)
   # Selects only rides corresponding to type, using a regular expression to find relevant rides
   if (type !="All") {
     if (type == "Class") {
-      data <- data%>%
-        filter(!grepl("*Just Ride", Title) & !grepl("*Scenic*", Title))
+      data <- dplyr::filter(data, !grepl("*Just Ride", Title) & !grepl("*Scenic*", Title))
     }
     else {
       data <- filter(data, grepl(paste0("*",type,"*"), data$Title))
