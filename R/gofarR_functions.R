f3d9b131d18f28bffb38ca1d06f3700f2f49f751
Erik Rehnberg <erik.m.rehnberg@gmail.com>
2020-06-30 11:17:28 -0400

    No errors, no warnings, license added, documentation added. 5 notes still

--- R/gofarR_functions.R
+++ R/gofarR_functions.R
@@ -15,71 +15,70 @@
 #'
 #' @export
 #'
-setup_peloton_data <- function(display_name = "None") {
-  if (display_name == "None") {
-    ouch <- "Bad input"
-    ouch
-  }
-  else workout_data_interface <<- read.csv(paste0(display_name,"_workouts.csv"))
+setup_peloton_data <- function(display_name) {
+  workout_data_interface <- read.csv(paste0(display_name,"_workouts.csv"))
+  data.frame(workout_data_interface)
 }
 
 #' @name top_instructor
 #' @title View top instructor
 #'
+#' @examples
+#' top_instructor()
+#' top_instructor(discipline = "Yoga")
+#'
 #' @export
-#' @param discipline
-top_instructor <- function(discipline = "All") {
+#' @param w_data - Workout data read in from setup_peloton_data
+#' @param discipline The class discipline to be summarized. Defaults to "All"
+top_instructor <- function(w_data = workout_data_interface, discipline = "All") {
   if (discipline == "All") {
-    filt_by_disc <- workout_data_interface%>%
-      filter(!grepl("*Just Ride", Title) & !grepl("*Scenic*", Title))
+    filt_by_disc <- dplyr::filter(w_data, !grepl("*Just Ride", w_data$Title) & !grepl("*Scenic*", w_data$Title))
   }
   else {
-    filt_by_disc <- workout_data_interface%>%
-      filter(!grepl("*Just Ride", Title) & !grepl("*Scenic*", Title))%>%
-      filter(Fitness.Discipline == discipline)
+    filt_by_disc <- dplyr::filter(w_data, !grepl("*Just Ride", w_data$Title) & !grepl("*Scenic*", w_data$Title) & w_data$Fitness.Discipline == discipline)
   }
-  dis_class_by_inst <- filt_by_disc$Instructor.Name%>%
-    table()%>%
-    data.frame()
+  dis_class_by_inst <- table(filt_by_disc["Instructor.Name"])
+  dis_class_by_inst <- data.frame(dis_class_by_inst)
   rank_inst <- dis_class_by_inst[rev(order(dis_class_by_inst$Freq)),]
-  top_inst <- head(rank_inst,1)
-  colnames(top_inst) <- c("Top Instructor", "Number of Classes Taken")
+  #new_column_names <- c("Top Instructor", "Number of Classes Taken")
+  #colnames(rank_inst) <- new_column_names
+  top_inst <- utils::head(rank_inst,1)
   print(top_inst, row.names = FALSE)
 }
 
 #' @title Cycling Summary
-#' @name cycling_summary()
+#' @name cycling_summary
 #' @description This function summarizes cycling data
-#' @usage cycling_summary(data = workout_data_interface, type = "All", min_time = 11, min_output = 50)
+#' @usage cycling_summary(w_data = workout_data_interface, type = "All", min_time = 11, min_output = 50)
 #'
-#' @param data - A formatted dataframe read in using setup_peloton_data. Defaults to a variable, "workout_data_interface", that is the output from that function.
+#' @param w_data - A formatted dataframe read in using setup_peloton_data. Defaults to a variable, "workout_data_interface", that is the output from that function.
 #' @param type Which type of
 #' @param min_time The minimum amount of time to be summarized, used to exclude short rides, either ones that were aborted or warmups/cooldowns.
 #' @param min_output The minimum output to be included in the summary. Defaults to 50 KJ
 #' @examples
+#'
 #' cycling_summary()
 #' cycling_summary(type = "Climb", min_time = 11)
 #'
 #' @return
 #' @export
 
-cycling_summary <- function(data = workout_data_interface,
+cycling_summary <- function(w_data = workout_data_interface,
                             type = "All",
                             min_time = 11,
                             min_output = 50) {
   # Selects only cycling rides which conform to min_time and min_output
-  data <- dplyr::filter(data, Fitness.Discipline == "Cycling" & Length..minutes. >= min_time & Total.Output >= min_output)
+  data <- dplyr::filter(w_data, w_data$Fitness.Discipline == "Cycling" & w_data$Length..minutes. >= min_time & w_data$Total.Output >= min_output)
   # Selects only rides corresponding to type, using a regular expression to find relevant rides
   if (type !="All") {
     if (type == "Class") {
-      data <- dplyr::filter(data, !grepl("*Just Ride", Title) & !grepl("*Scenic*", Title))
+      data <- dplyr::filter(data, !grepl("*Just Ride", data$Title) & !grepl("*Scenic*", data$Title))
     }
     else {
       data <- filter(data, grepl(paste0("*",type,"*"), data$Title))
     }
   }
   else data
-
 data
 
 
